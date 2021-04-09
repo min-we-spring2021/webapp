2b14c4b2e50c3c737c4f80c41eae00e756855c98
wenhao <minwenhao1992@gmail.com>
2021-04-08 19:19:27 -0700

    update sns

--- app.js
+++ app.js
@@ -22,10 +22,10 @@ const client = require('./_helpers/client');
 
 const aws = require("aws-sdk");
 const { propfind } = require('./users/users.controller');
-// aws.config.update({
-//     accessKeyId: process.env.accessKeyId,
-//     secretAccessKey: process.env.secretAccessKey
-// });
+aws.config.update({
+    accessKeyId: process.env.accessKeyId,
+    secretAccessKey: process.env.secretAccessKey
+});
 aws.config.update({ region: 'us-east-1' });
 const s3 = new aws.S3();
 
